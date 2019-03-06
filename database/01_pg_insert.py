856b1974aa69fcaa49ade3c9adbfb33d708cea95
Patrick Gichini <Patogichini254@gmail.com>
2019-03-06 07:51:27 +0300

    Update 01_pg_insert.py

--- database/01_pg_insert.py
+++ database/01_pg_insert.py
@@ -1,6 +1,6 @@
 #On your local postgres database
-#Create a table called girlriends, have four columns namely
-#id (as serial that auto generate), name (a varchar),age(int) and height(double)
+#Create a table called girlriends, have five columns namely
+#id (as serial that auto generate), girl_code(a varchar code that is unique),name (a varchar),age(int) and height(double)
 #Write a python script that allows you to connect to the db and insert a new girlfriend
 #Dont we all wish getting girlfriends was that easy :-)
 #Your code goes here
